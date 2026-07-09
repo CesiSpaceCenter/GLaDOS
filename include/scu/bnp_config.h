64e37465443b9a9715c088fca06b0f74a5540349
Enzo PB <enzo.puiroux@mail.com>
2026-07-09 17:43:12 +0200

    fix: reinstate correct config & build flags

--- include/scu/bnp_config.h
+++ include/scu/bnp_config.h
@@ -1,16 +1,16 @@
 #include "../boards/DFR_F722_DUAL_HD.h"
 
 #define I2C1_ENABLE false
-#define I2C2_ENABLE false
+#define I2C2_ENABLE true
 #define I2C3_ENABLE false
 
-#define SPI1_ENABLE false
+#define SPI1_ENABLE true
 #define SPI2_ENABLE false
-#define SPI3_ENABLE false
+#define SPI3_ENABLE true
 
-#define UART1_ENABLE true
-#define UART2_ENABLE true
-#define UART3_ENABLE true
-#define UART4_ENABLE true
-#define UART5_ENABLE true
-#define UART6_ENABLE true
+#define UART1_ENABLE false
+#define UART2_ENABLE false
+#define UART3_ENABLE false
+#define UART4_ENABLE false
+#define UART5_ENABLE false
+#define UART6_ENABLE false
